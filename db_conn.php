79e6853c02b0c5644a455a2988cf89a689701b16
ElOtroNew123 <91492097+ElOtroNew123@users.noreply.github.com>
2021-12-18 14:56:15 -0600

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,14 +1,14 @@
 <?php  
 
-$sname = "localhost";
-$uname = "root";
-$password = "";
+$sname = "sql307.epizy.com";
+$uname = "epiz_30464368";
+$password = "MqNIeGRgETYiX";
 
-$db_name = "test_db";
+$db_name = "epiz_30464368_prima";
 
 $conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
-	echo "Connection failed!";
+	echo "Conexión fallida!";
 	exit();
 }
